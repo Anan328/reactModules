16c4d8f29e621bf34dff77b10c5ddd83c404dc17
ANAN <ananshah328@gmail.com>
2024-09-08 16:12:37 +0530

    hooks

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter,setCounter] = useState(15);
+  // let counter = 15;
+  const addValue = ()=>{
+  // counter+=1;
+  if(counter < 20){
+    setCounter(counter+=1)
+  }
+  //console.log(counter)
+  }
+
+  const decValue = ()=>{
+    if(counter != 0){
+      setCounter(counter-=1)
+    }
+  }
+
+  return (
+    <>
+     <h1>React App</h1>
+     <h2>Counter value: {counter}</h2>
+     <button onClick={addValue}>Add value</button>
+     <br />
+     <button onClick={decValue}>Decrease value</button>
+    </>
+  )
+}
+
+export default App
