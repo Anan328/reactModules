b74c3172e835aeee8626b5c021dd90ac37f1a24f
ANAN <ananshah328@gmail.com>
2024-09-10 15:23:53 +0530

    added copy button

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,11 +1,13 @@
-import { useState, useCallback, useEffect} from 'react'
+import { useState, useCallback, useEffect, useRef} from 'react'
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
-
+  // useRef hook
+  const passwordRef = useRef(null);
+  // useCallback is for optimization purpose only, we can also work without this
   const passwordGenertor = useCallback(()=>{
     let pass = '';
     let str = 'ABCDEFGHIJKLMNOFQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
@@ -19,17 +21,32 @@ function App() {
     }
     setPassword(pass)
 
-  },[length,numberAllowed,charAllowed,setPassword]);
+  },[length,numberAllowed,charAllowed,setPassword])
+  const copyClipboard = useCallback(()=>{
+    passwordRef.current?.select();
+    // passwordRef.current?.setSelectionRange(0,5)
+    window.navigator.clipboard.writeText(password)
+
+  },[password])
   useEffect(()=>{
     passwordGenertor()
-  },[length,numberAllowed,charAllowed,passwordGenertor])
+  },[length,numberAllowed,charAllowed,passwordGenertor]) // dont add passwordGenertor if not using useCallback
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg py-6 px-8 my-8 text-orange-500 bg-gray-400'>
       <h1 className='text-4xl text-center text-white mb-2 my-3'>Password Generator</h1>
         <div className='flex shadow rounded-lg overflow-hidden mb-4'>
-        <input type="text"  value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly/>
-        <button className='outline-none rounded-xl bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
+        <input 
+        type="text"  
+        value={password} 
+        className='outline-none w-full py-1 px-3' 
+        placeholder='password' 
+        readOnly 
+        ref={passwordRef}
+        />
+        <button 
+        onClick={copyClipboard}
+        className='outline-none  bg-blue-700 text-white px-3 py-0.5 shrink-0'>Copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
