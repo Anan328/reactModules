7eaed431932678ce5f79dea3f5e1d2b8a60de15f
ANAN <ananshah328@gmail.com>
2024-09-12 23:02:53 +0530

    almost finish

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import { Route, Router, Routes } from 'react-router'
+import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   return (
