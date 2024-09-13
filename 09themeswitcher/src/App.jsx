fb69e657559e101ab275a8f12d18dc10978e4d43
ANAN <ananshah328@gmail.com>
2024-09-13 22:42:08 +0530

    finished

--- 09themeswitcher/src/App.jsx
+++ 09themeswitcher/src/App.jsx
@@ -1,8 +1,36 @@
+import ThemeBtn from "./components/Button"
+import Card from "./components/Card"
+import { useState,useEffect } from "react"
+import { ThemeProvider } from "./context/Theme"
+
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
   return (
-    <>
-    <h1 className="text-2xl text-center mt-4 underline">Context API</h1>
-    </>
+  <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+                      <div className="w-full">
+                          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                              <ThemeBtn/>
+                          </div>
+
+                          <div className="w-full max-w-sm mx-auto">
+                            <Card/>
+                          </div>
+                      </div>
+      </div>
+  </ThemeProvider>
   )
 }
 
